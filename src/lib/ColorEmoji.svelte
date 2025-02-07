840f981f3a6a4fb1d2a89b6f33b7e40d2e39ee80
Koichi Yamamoto <noughts@gmail.com>
2025-02-07 14:13:45 +0900

    initial commmit

---
+++ src/lib/ColorEmoji.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let emoji: string;
+    export let size: string;
+    $: codePoint = emoji.codePointAt(0);
+    $: unicode = codePoint?.toString(16);
+    $: src = `https://raw.githubusercontent.com/googlefonts/noto-emoji/refs/heads/main/svg/emoji_u${unicode}.svg`;
+</script>
+
+<img {src} alt={emoji} style:width={size} style:height={size} />
