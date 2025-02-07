dd20d17eb82d0b26ecb0f28ac4a57dffa768fab1
Koichi Yamamoto <noughts@gmail.com>
2025-02-07 15:52:06 +0900

    さらに明示的になるようにリネーム

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,2 @@
 // Reexport your entry components here
-export { default as NotoEmoji } from "./NotoEmoji.svelte";
+export { default as NotoColorEmoji } from "./NotoColorEmoji.svelte";
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import NotoEmoji from "$lib/NotoEmoji.svelte";
+    import NotoColorEmoji from "$lib/NotoColorEmoji.svelte";
 </script>
 
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<NotoEmoji emoji="📅" size="22px" />
-<NotoEmoji emoji="🥹" size="44px" />
+<NotoColorEmoji emoji="📅" size="22px" />
+<NotoColorEmoji emoji="🥹" size="44px" />
